ac91aec44bd7fe63287020481a80d1cc5ed3f531
alexhisen <ahisen@am.net>
2017-02-07 14:25:08 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # modules-values-extract
+
+Extracts all CSS Module @values from a list of css files into an object.
+
+This allows you to feed them, for example, to postcss-custom-properties in webpack.config.js
+
+## Installation
+`npm install modules-values-extract --save-dev`
+
+Depends on `postcss` as a peer dependency.
+
+## Usage
+In your webpack.config.js:
+```js
+const fs = require('fs');
+const path = require('path');
+const extractValues = require('modules-values-extract');
+
+// get all *.css files in current directory:
+const cssFiles = fs.readdirSync(__dirname)
+  .filter((file) => file.match(/\.css/i))
+  .map((file) => path.resolve(__dirname, file));
+
+let config;
+let myVariables;
+
+module.exports = extractValues({
+ files: cssFiles,
+ /* optional list of plugin functions to use in processing, for example: */
+ /* eslint-disable global-require */
+ plugins: [require('postcss-calc'), require('precss')],
+ /* eslint-enable global-require */
+}).then((variables) => {
+  myVariables = variables;
+  return config;
+});
+
+config = {
+  // ... various webpack config
+
+  postcss: () => {
+    return [
+      /* eslint-disable global-require */
+      require('postcss-cssnext')({
+        features: {
+          customProperties: {
+            variables: myVariables,
+          },
+        },
+      }),
+      require('postcss-modules-values'),
+      /* eslint-enable global-require */
+    ];
+  },
+};
+```
+
+Note that the above module.exports in wepback.config.js is actually a Promise that resolves to the config object, not the config object itself.
+
+This requires webpack-dev-server 2.x or 1.16.3+.
---
+++ index.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const fs = require('fs');
+const postcss = require('postcss');
+const moduleValuesPlugin = require('postcss-modules-values-replace');
+
+/**
+ * Extracts all CSS Module @values from a list of css files into an object.
+ * @param {Object} options
+ * @param {Array} options.files - list of fully-qualified css file names to extract @values from, for example for all .css files current dir:
+ *  const path = require('path');
+ *  const cssFiles = fs.readdirSync(__dirname).filter((file) => file.match(/\.css/i)).map((file) => path.resolve(__dirname, file));
+ * @param {Array} [options.plugins] - list of postcss plugin functions to use in processing, i.e.:
+ *                                    [require('postcss-calc'), require('precss')]
+ * @returns {Promise|Object} - returns a promise which resolves to an object with values from all files combined.
+ */
+function extractValues(options = {}) {
+  const variables = {};
+  const plugins = options.plugins || [];
+  const processor = postcss([moduleValuesPlugin].concat(plugins));
+  const files = options.files || [];
+
+  return new Promise((resolve, reject) => {
+    const promises = [];
+    files.forEach((file) => {
+      const css = fs.readFileSync(file);
+      // from/to below required by postcss for sourceMap, not actually used to read/write file:
+      promises.push(processor
+        .process(css, { from: file, to: `${file}.js` })
+        .then(result => {
+          if (result.messages[0]) {
+            Object.assign(variables, result.messages[0].value);
+          }
+        }));
+    });
+    Promise.all(promises).then(() => {
+      resolve(variables);
+    }).catch(reject);
+  });
+}
+
+module.exports = extractValues;
